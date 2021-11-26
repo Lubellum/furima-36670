263c0ac67a84347cffe190502920956c98fd70c5
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-26 12:45:22 +0900

    rails db:migrate:resetの実行
    保存したマイグレーションファイルがDBに保存されていなかったため

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,13 @@
 ActiveRecord::Schema.define(version: 2021_11_24_021413) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name_kanji", null: false
+    t.string "last_name_kanji", null: false
+    t.string "first_name_katakana", null: false
+    t.string "last_name_katakana", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
