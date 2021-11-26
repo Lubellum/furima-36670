cd0bd27352596b8f6044ccfdee9c0fb57da2e31d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-26 18:45:41 +0900

    各カラムにnull: falseのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  
+  validates :nickname            , null: false
+  validates :first_name_kanji    , null: false
+  validates :last_name_kanji     , null: false
+  validates :first_name_katakana , null: false
+  validates :last_name_katakana  , null: false
+  validates :birth_date          , null: false
 end
