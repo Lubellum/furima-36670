9b3080b9225cf4233813b1282fc64eca63e7288c
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-26 12:52:19 +0900

    birth_dateカラムの追記・反映
    記述忘れのため

--- db/migrate/20211124021413_devise_create_users.rb
+++ db/migrate/20211124021413_devise_create_users.rb
@@ -11,6 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_kanji      , null: false
       t.string :first_name_katakana  , null: false
       t.string :last_name_katakana   , null: false
+      t.date   :birth_date           , null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_021413) do
     t.string "last_name_kanji", null: false
     t.string "first_name_katakana", null: false
     t.string "last_name_katakana", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
