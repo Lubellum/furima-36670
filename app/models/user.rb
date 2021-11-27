20b4bed09657711ede007ddcb571712076ae04a3
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-27 21:04:50 +0900

    名前入力欄のバリデーション(カタカナ)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname            , null: false
   validates :first_name_kanji    , null: false, format: { with: /\A[一-龥]+\z/ }
   validates :last_name_kanji     , null: false, format: { with: /\A[一-龥]+\z/ }
-  validates :first_name_katakana , null: false
-  validates :last_name_katakana  , null: false
+  validates :first_name_katakana , null: false, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :last_name_katakana  , null: false, format: { with: /\A([ァ-ン]|ー)+\z/ }
   validates :birth_date          , null: false
 end
