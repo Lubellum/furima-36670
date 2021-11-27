0e047ca995838fb42b298a2f872ce1d3f7949433
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-27 21:01:43 +0900

    名前入力欄のバリデーション(漢字)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname            , null: false
-  validates :first_name_kanji    , null: false
-  validates :last_name_kanji     , null: false
+  validates :first_name_kanji    , null: false, format: { with: /\A[一-龥]+\z/ }
+  validates :last_name_kanji     , null: false, format: { with: /\A[一-龥]+\z/ }
   validates :first_name_katakana , null: false
   validates :last_name_katakana  , null: false
   validates :birth_date          , null: false
