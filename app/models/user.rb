1a669a8e94160cc7320b87c7d4c185d19d6e6df8
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-29 19:45:30 +0900

    エラーメッセージの追加
    入力された要素にどの様な不備があるかユーザーに把握させるため

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname            , presence: true
-  validates :first_name_kanji    , presence: true, format: { with: /\A[一-龥]+\z/ }
-  validates :last_name_kanji     , presence: true, format: { with: /\A[一-龥]+\z/ }
-  validates :first_name_katakana , presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
-  validates :last_name_katakana  , presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :password            , format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+  validates :first_name_kanji    , presence: true, format: { with: /\A[一-龥]+\z/, message: "is invalid. Input full-width characters" }
+  validates :last_name_kanji     , presence: true, format: { with: /\A[一-龥]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name_katakana , presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name_katakana  , presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :birth_date          , presence: true
 end
