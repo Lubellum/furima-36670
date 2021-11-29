7d4d83610a7078b2f6fc5101df65fd87eece9e72
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-29 20:47:41 +0900

    テスト項目の羅列

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context 'ユーザー情報' do
+      it 'ニックネームが必須である' do
+        
+      end
+      it 'メールアドレスが必須である' do
+        
+      end
+      it 'メールアドレスが一意性である' do
+        
+      end
+      it 'メールアドレスは、@を含む必要がある' do
+        
+      end
+      it 'パスワードが必須である' do
+        
+      end
+      it 'パスワードは、6文字以上での入力が必須である' do
+        
+      end
+      it 'パスワードは、半角英数字混合での入力が必須である' do
+        
+      end
+      it 'パスワードとパスワード（確認）は、値の一致が必須である' do
+        
+      end
+    end
+
+    context '本人確認情報' do
+      it 'お名前(全角)は、名字と名前がそれぞれ必須である' do
+        
+      end
+      it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須である' do
+        
+      end
+      it 'お名前カナ(全角)は、名字と名前がそれぞれ必須である' do
+        
+      end
+      it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須である' do
+        
+      end
+      it '生年月日が必須である' do
+        
+      end
+    end
+  end
 end
