1ab63c5d6266fae8711cf2b173f078f98b4f5046
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-01 20:29:55 +0900

    全角のバリデーションを漢字のみ許可から、ひらがなとカタカナを許可に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   validates :first_name_kanji, presence: true,
-                               format: { with: /\A[一-龥]+\z/, message: 'is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :last_name_kanji, presence: true,
-                              format: { with: /\A[一-龥]+\z/, message: 'is invalid. Input full-width characters' }
+                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :first_name_katakana, presence: true,
                                   format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :last_name_katakana, presence: true,
