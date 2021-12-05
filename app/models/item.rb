1f4ff204f14e81c22953ce41ca4357816fd169a0
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-05 20:06:09 +0900

    ActiveHashの実装(配送料負担側)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :delivery_fee_payment
 
   has_one_attached :image
 end
