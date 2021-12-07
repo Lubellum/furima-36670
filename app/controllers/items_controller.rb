698ad9ba30a19c1a2cac469455f7cbb6c0035e82
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-07 12:37:25 +0900

    商品投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    # binding.pry
+    @item = Item.create(item_params)
+    if @item.save 
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, 
+                                 :category_id, :condition_id, :delivery_fee_payment_id, 
+                                 :ship_from_address_id, :ship_to_duration_id, :explanation, :price)
+                                 .merge(user_id: current_user.id)
   end
 end
