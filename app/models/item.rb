dae591300356ad559285dbeade038ea1588d59ef
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-07 12:37:04 +0900

    商品情報のバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,24 @@ class Item < ApplicationRecord
   belongs_to :ship_to_duration
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_payment_id
+    validates :ship_from_address_id
+    validates :ship_to_duration_id
+    validates :price
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_payment_id
+      validates :ship_from_address_id
+      validates :ship_to_duration_id
+    end
+  end
 end
