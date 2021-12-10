e8931756fcc1905233a117bbb2d7ac7d0b84d1e6
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-10 14:15:54 +0900

    FactoryBotにimage要素の記述
    FactoryBotで画像を使用する際はafterで要素を追加する必要があるため

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image                   {  }
     item_name               {  }
     explanation             {  }
     category_id             {  }
@@ -9,5 +8,9 @@ FactoryBot.define do
     ship_from_address_id    {  }
     ship_to_duration_id     {  }
     price
+
+    after(:build) do |item|
+      item.imgage.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
