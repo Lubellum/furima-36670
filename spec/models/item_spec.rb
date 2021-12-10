febcaba78082f87195167330bf29a27d034e44e2
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-10 18:29:05 +0900

    テスト　発送までの日数の選択はidが1以外の必要がある

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -122,8 +122,13 @@ RSpec.describe Item, type: :model do
         it '発送までの日数の選択が必須' do
           @item.ship_to_duration_id = ''
           @item.valid?
-          binding.pry
           expect(@item.errors.full_messages).to include("Ship to duration can't be blank")
+        end
+        it '発送までの日数の選択はidが1以外の必要がある' do
+          @item.ship_to_duration_id = '1'
+          @item.valid?
+          binding.pry
+          expect(@item.errors.full_messages).to include("Ship to duration must be other than 1")
         end
       end
     end
