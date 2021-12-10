954763ba97adc3b537c89c5f802489414e6f9140
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-10 14:41:54 +0900

    item_specの大枠作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    describe '商品情報' do
+      context '商品出品が出来る時' do
+        
+      end
+
+      context '商品出品が出来ない時' do
+        
+      end
+    end
+
+    describe '商品情報(ActiveHash)' do
+      
+    end
+  end
 end
