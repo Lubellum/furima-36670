db424bccf39abd506b5ace7b40011cb4e622664c
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-10 12:46:33 +0900

    itemモデルのFactoryBot作成(雛形)

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image                   {  }
+    item_name               {  }
+    explanation             {  }
+    category_id             {  }
+    condition_id            {  }
+    delivery_fee_payment_id {  }
+    ship_from_address_id    {  }
+    ship_to_duration_id     {  }
+    price
   end
 end
