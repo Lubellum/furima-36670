ee90c1f3df4e588fc38455c29803d2455dcbd0d8
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-10 17:05:34 +0900

    販売価格の補足コメントを変更
    「以上」とする場合、10,000,000円の方が適切なため

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
         end
-        it '販売価格は300円〜9,999,999円の範囲での入力が必須である(9,999,999円以上)' do
+        it '販売価格は300円〜9,999,999円の範囲での入力が必須である(10,000,000円以上)' do
           @item.price = '10000000'
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
