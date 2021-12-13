0b4ccaef900c7f1fea7ccbb8ca033b92a193d44b
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-13 21:19:38 +0900

    商品を投稿したユーザーにのみ商品削除を可能とする(ログイン状態でのみ許可)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user == @item.user
+    if user_signed_in? && current_user == @item.user
       @item.destroy
       redirect_to root_path
     else
