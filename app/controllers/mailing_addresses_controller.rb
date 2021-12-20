c85dd498f18ebd10a893098198c7a82ed025a67d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-20 20:28:47 +0900

    購入者情報の入力処理

--- app/controllers/mailing_addresses_controller.rb
+++ app/controllers/mailing_addresses_controller.rb
@@ -1,4 +1,19 @@
 class MailingAddressesController < ApplicationController
   def index
   end
+
+  def create
+    @mailing_address = MailingAddress.new(mailing_address_params)
+    if @mailing_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  private
+  def mailing_address_params
+    params.permit(:postal_code, :ship_from_address_id, :municipalities, 
+                                            :street_number, :building_name, :telephone_number)
+  end
 end
