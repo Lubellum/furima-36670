7449a631471e01be11a019ad24ddc888bdaae8c5
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-21 21:28:46 +0900

    バリデーションの記述

--- app/models/purchase_mailing.rb
+++ app/models/purchase_mailing.rb
@@ -1,3 +1,14 @@
 class PurchaseMailing
   include ActiveModel::Model
+  attr_accessor :postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validetes :postal_code          , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validetes :ship_from_address_id , numericality: {other_than: 0, message: "can't be blank"}
+    validetes :municipalities
+    validetes :street_number
+    validetes :telephone_number     , format: {with: /\A[0-9]{11}\z/}
+    validetes :user_id
+    validetes :item_id
+  end
 end
