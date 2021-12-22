e69ffe3e5a1b6a7bb38e5cfe31bf85b8049f365c
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-22 12:48:50 +0900

    mailing_addresテーブルのマイグレーション実行

--- db/migrate/20211222012310_create_mailing_addres.rb
+++ db/migrate/20211222012310_create_mailing_addres.rb
@@ -1,7 +1,13 @@
 class CreateMailingAddres < ActiveRecord::Migration[6.0]
   def change
     create_table :mailing_addres do |t|
-
+      t.string     :postal_code          , null: false
+      t.integer    :ship_from_address_id , null: false
+      t.string     :municipalities       , null: false
+      t.string     :street_number        , null: false
+      t.string     :building_name
+      t.string     :telephone_number     , null: false
+      t.references :purchase_record      , null: false, foreign_key: true
       t.timestamps
     end
   end
