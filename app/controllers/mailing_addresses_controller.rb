21ce201c291b2e245d01cd8fc8c6fa6af040c9d5
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-27 19:20:13 +0900

    保存先をMilingAddressモデルからPurchaseMailingに修正

--- app/controllers/mailing_addresses_controller.rb
+++ app/controllers/mailing_addresses_controller.rb
@@ -1,8 +1,23 @@
 class MailingAddressesController < ApplicationController
   def index
-    @mailing_address = MailingAddress.new
+  end
+
+  def new
+    @purchase_mailing = PurchaseMailing.new
   end
 
   def create
+    @purchase_mailing = PurchaseMailing.new(purchase_mailing_params)
+    if @purchase_mailing.valid?
+      @purchase_mailing.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_mailing_params
+    params.require(:purchase_mailing).permit(:postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id)
   end
 end
