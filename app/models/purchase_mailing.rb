4d71037dfd2c6c39108a642175472c239225d517
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-27 18:48:27 +0900

    Formオブジェクトの保存の記述をnewからcreateに修正
    newでは情報が保存されないため、createが正しい。

--- app/models/purchase_mailing.rb
+++ app/models/purchase_mailing.rb
@@ -13,8 +13,8 @@ class PurchaseMailing
   end
 
   def save
-    purchase = PurchaseRecord.new(user_id: user_id, item_id: item_id)
-    MailingAddress.new(postal_code: postal_code, ship_from_address_id: ship_from_address_id, municipalities: municipalities, street_number: street_number,
+    purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    MailingAddress.create(postal_code: postal_code, ship_from_address_id: ship_from_address_id, municipalities: municipalities, street_number: street_number,
                        building_name: building_name, telephone_number: telephone_number, user_id: purchase.id, item_id: purchase.id)
   end
 end
