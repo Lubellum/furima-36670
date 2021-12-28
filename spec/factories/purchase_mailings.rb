b0e1e1a96851940a4224cf2e4cd1dd66963c0a47
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 08:27:45 +0900

    FactoryBotの記述

--- spec/factories/purchase_mailings.rb
+++ spec/factories/purchase_mailings.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchase_mailing do
-    
+    address = Gimei.address
+
+    postal_code          { '123-4567' }
+    ship_from_address_id { 2 }
+    municipalities       { address.kanji }
+    street_number        { address.kanji }
+    building_name        { 'マルコポーロ333' }
+    telephone_number     { Faker::Number.between(from: 1234567890, to: 12345678901) }
   end
 end
