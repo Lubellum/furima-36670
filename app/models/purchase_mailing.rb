c27ba101a56cb8caec89215152669d18776cff10
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-28 18:36:55 +0900

    saveメソッドのendを追記及びvalidatesの綴りを修正

--- app/models/purchase_mailing.rb
+++ app/models/purchase_mailing.rb
@@ -3,17 +3,18 @@ class PurchaseMailing
   attr_accessor :postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
-    validetes :postal_code          , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validetes :ship_from_address_id , numericality: {other_than: 0, message: "can't be blank"}
-    validetes :municipalities
-    validetes :street_number
-    validetes :telephone_number     , format: {with: /\A[0-9]{11}\z/}
-    validetes :user_id
-    validetes :item_id
+    validates :postal_code          , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :ship_from_address_id , numericality: {other_than: 0, message: "can't be blank"}
+    validates :municipalities
+    validates :street_number
+    validates :telephone_number     , format: {with: /\A[0-9]{11}\z/}
+    validates :user_id
+    validates :item_id
   end
 
   def save
     purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     MailingAddress.create(postal_code: postal_code, ship_from_address_id: ship_from_address_id, municipalities: municipalities, street_number: street_number,
                        building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+  end
 end
