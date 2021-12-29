e6e027a9cfb6db67cbc753f4fd1ddee94167bba1
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 09:05:14 +0900

    purchase_mailing_specの雛形作成

--- spec/models/purchase_mailing_spec.rb
+++ spec/models/purchase_mailing_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseMailing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase_mailing = FactoryBot.build(:purchase_mailing)
+  end
+
+  describe '商品購入' do
+    context '商品購入が出来る時' do
+      
+    end
+
+    context '商品購入が出来ない時' do
+      
+    end
+  end
 end
