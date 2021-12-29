d76386ba884730b6e0246f7b4754f8d182c58bdf
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 11:35:08 +0900

    都道府県選択のnumericalityを0から1に修正及びエラーメッセージの変更

--- app/models/purchase_mailing.rb
+++ app/models/purchase_mailing.rb
@@ -4,7 +4,7 @@ class PurchaseMailing
 
   with_options presence: true do
     validates :postal_code          , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :ship_from_address_id , numericality: {other_than: 0, message: "can't be blank"}
+    validates :ship_from_address_id , numericality: {other_than: 1}
     validates :municipalities
     validates :street_number
     validates :telephone_number     , format: {with: /\A[0-9]{10,11}\z/}
