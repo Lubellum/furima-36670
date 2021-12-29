1d252c9988d4fa0b5febcbe0424f390c448a119d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 09:39:50 +0900

    テスト　建物名が無くても購入できる

--- spec/models/purchase_mailing_spec.rb
+++ spec/models/purchase_mailing_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe PurchaseMailing, type: :model do
       it '郵便番号、都道府県、市区町村、番地、建物名、電話番号があれば購入が出来る' do
         expect(@purchase_mailing).to be_valid
       end
+      it '建物名が無くても購入できる' do
+        @purchase_mailing.building_name = nil
+        expect(@purchase_mailing).to be_valid
+      end
     end
 
     context '商品購入が出来ない時' do
