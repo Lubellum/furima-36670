91d88ef9345c457ac00e7e1fba2c808ed81bcc8f
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 14:31:11 +0900

    購入情報に商品idの紐付け

--- app/controllers/mailing_addresses_controller.rb
+++ app/controllers/mailing_addresses_controller.rb
@@ -15,6 +15,6 @@ class MailingAddressesController < ApplicationController
 
   private
   def purchase_mailing_params
-    params.require(:purchase_mailing).permit(:postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id)
+    params.require(:purchase_mailing).permit(:postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item_id)
   end
 end
