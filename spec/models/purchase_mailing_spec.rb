d6deea6d5f30f6ca96f5825856cd784ab67805e8
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-12-29 09:25:24 +0900

    @purchase_mailingにユーザー情報と商品情報を紐付け
    purchase_mailingクラスはモデルでは無いため、FactoryBot内ではなくbeforeアクションとして定義する必要がある

--- spec/models/purchase_mailing_spec.rb
+++ spec/models/purchase_mailing_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe PurchaseMailing, type: :model do
   before do
-    @purchase_mailing = FactoryBot.build(:purchase_mailing)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_mailing = FactoryBot.build(:purchase_mailing, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入' do
     context '商品購入が出来る時' do
-      
+      it '郵便番号、都道府県、市区町村、番地、建物名、電話番号があれば購入が出来る' do
+        expect(@purchase_mailing).to be_valid
+      end
     end
 
     context '商品購入が出来ない時' do
