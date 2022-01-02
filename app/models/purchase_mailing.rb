f2eb29b8ca322956275d73f986d71dcfeed23716
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-02 12:59:20 +0900

    保存先カラム名をpurchase_record_idに修正

--- app/models/purchase_mailing.rb
+++ app/models/purchase_mailing.rb
@@ -15,6 +15,6 @@ class PurchaseMailing
   def save
     purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     MailingAddress.create(postal_code: postal_code, ship_from_address_id: ship_from_address_id, municipalities: municipalities, street_number: street_number,
-                       building_name: building_name, telephone_number: telephone_number, purchase_record: purchase.id)
+                       building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase.id)
   end
 end
