5499c04e9acc2a9a86a03b33f1f148c380a4db1d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-03 21:21:40 +0900

    payjpの決済処理(コントローラー)

--- app/controllers/mailing_addresses_controller.rb
+++ app/controllers/mailing_addresses_controller.rb
@@ -8,6 +8,7 @@ class MailingAddressesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_mailing = PurchaseMailing.new(purchase_mailing_params)
     if @purchase_mailing.valid?
+      pay_item
       @purchase_mailing.save
       redirect_to root_path
     else
@@ -18,5 +19,14 @@ class MailingAddressesController < ApplicationController
   private
   def purchase_mailing_params
     params.require(:purchase_mailing).permit(:postal_code, :ship_from_address_id, :municipalities, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "秘密鍵を記述"
+    Payjp::Charge.create(
+      amount: purchase_mailing_params[:price],
+      card:   purchase_mailing_params[:token],
+      currency: 'jpy'
+    )
   end
 end
